c8a7ace4c2501987efeec61ddc376dae2e84f0d6
pishchynski <pavelalexbel@outlook.com>
2019-01-30 13:35:07 +0300

    rewriting Ramaswami matrices calculation in strict accordance to given algorithm:
    rewrote main algorithm part

--- ramaswami.py
+++ ramaswami.py
@@ -104,7 +104,7 @@ def calc_ramaswami_matrices(matr_S: np.ndarray, matr_tilde_S: np.ndarray, vect_b
     """
     matr_U = []
     matr_L = []
-    matr_A = [[[0]]]  # we start from A_1
+    matr_A = [np.array([[0]])]  # we start from A_1
     matr_P1 = [0 for _ in range(N)] if N > 0 else [0]
     M = matr_S.shape[0]  # number of phases
     matr_tau = [0]
@@ -167,42 +167,58 @@ def calc_ramaswami_matrices(matr_S: np.ndarray, matr_tilde_S: np.ndarray, vect_b
                     matr_A[m] = matr_Az[m]
 
         print('Calculating P_1_i(beta)')
+
         for j in range(M - 1):
-            a = []
+            a = [[]]
+
             if j != 0:
                 a = np.array([[0 for _ in range(j + 1)]])
+
                 for k in range(M - j - 1, M):
-                    a[0][k - M + j + 1] = vect_beta[0][k]
+                    a[0][k - M + j + 1] = vect_beta[0][k]  # beta_g_1 ???
+
             matr_Pz = [0 for _ in range(N)]
 
             for m in range(1, N):
                 if j == 0:
                     temp = np.array([[0 for _ in range(m + 2)] for _ in range(m + 1)])
+
                     for l in range(m + 1):
                         temp[l][l + 1] = vect_beta[0][M - 1]
                         temp[l][l] = vect_beta[0][M - 2]
+
                     matr_P1[m] = temp
                 else:
                     m_A = 1
                     n_A = 1
+
                     for k in range(1, m + 1):
                         m_A += matr_P1[k].shape[0]
                         n_A += matr_P1[k].shape[1]
+
                     n_A += a[0].shape[0]
                     temp = np.array([[0 for _ in range(n_A)] for _ in range(m_A)])
 
                     m_pos = 1
                     n_pos = 1
-                    temp[0][0] = vect_beta[0][M - j - 2]
+
+                    temp[0][0] = vect_beta[0][M - j - 2]  # beta1 ???
+
                     copy_matrix_block(temp, a, 0, 1)
 
                     for l in range(1, m + 1):
                         shift = matr_P1[l].shape[0]
+
                         copy_matrix_block(temp, vect_beta[0][M - j - 2] * np.identity(shift), m_pos, n_pos)
+
                         n_pos += shift
+
                         copy_matrix_block(temp, matr_P1[l], m_pos, n_pos)
+
                         m_pos += shift
+
                     matr_Pz[m] = temp
+
             if j != 0:
                 for m in range(1, N):
                     matr_P1[m] = matr_Pz[m]
@@ -211,6 +227,7 @@ def calc_ramaswami_matrices(matr_S: np.ndarray, matr_tilde_S: np.ndarray, vect_b
         matr_P1[0] = temp
 
         print('Calculating L_i(N, matr_tilde_S)')
+
         matr_L = get_matr_LzNT(matr_tilde_S, N)
     else:
         matr_A = [0 for _ in range(N + 1)]
@@ -218,8 +235,8 @@ def calc_ramaswami_matrices(matr_S: np.ndarray, matr_tilde_S: np.ndarray, vect_b
         matr_P1 = [0 for _ in range(N)]
 
         for n in range(N + 1):
-            matr_A[n] = n * np.matmul(matr_S, np.identity(1))
+            matr_A[n] = n * np.dot(matr_S, np.identity(1))
             if n != N:
-                matr_L[n] = (-N + n) * np.matmul(matr_S, np.identity(1))
+                matr_L[n] = (-N + n) * np.dot(matr_S, np.identity(1))
                 matr_P1[n] = np.identity(1)
     return matr_L, matr_A, matr_P1
