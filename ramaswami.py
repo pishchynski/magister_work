fixed stupid bugs i.e. possible storing integers instead of floats which causes calculation errors

ramaswami.py
@@ -28,7 +28,7 @@ def get_matr_LzNT(matr_T: np.ndarray, N: int):
 
                 m_A += matr_L[N - 1].shape[1]  # added same num of rows as cols because matrix is square
 
-                temp = np.array([[0 for _ in range(n_A)] for _ in range(m_A)])  # elements of matrix to use positions
+                temp = np.array([[0 for _ in range(n_A)] for _ in range(m_A)], dtype=float)  # elements of matrix to use positions
 
                 n_pos = 0
                 m_pos = 0
@@ -120,7 +120,7 @@ def calc_ramaswami_matrices(matr_S: np.ndarray, matr_tilde_S: np.ndarray, vect_b
     """
     matr_U = []
     matr_L = []
-    matr_A = [np.array([[0]])]  # we start from A_1
+    matr_A = [np.array([[0]], dtype=float)]  # we start from A_1
     matr_P1 = [0 for _ in range(N)] if N > 0 else [0]
     M = matr_S.shape[0]  # number of phases
     matr_tau = [0]
@@ -139,7 +139,7 @@ def calc_ramaswami_matrices(matr_S: np.ndarray, matr_tilde_S: np.ndarray, vect_b
 
             for m in range(1, N + 1):
                 if j == 0:
-                    temp = np.array([[0 for _ in range(m + 1)] for _ in range(m + 1)])
+                    temp = np.array([[0 for _ in range(m + 1)] for _ in range(m + 1)], dtype=float)
 
                     for l in range(m):
                         temp[l][l + 1] = (m - l) * matr_S[M - 2][M - 1]
@@ -157,7 +157,7 @@ def calc_ramaswami_matrices(matr_S: np.ndarray, matr_tilde_S: np.ndarray, vect_b
                     m_A += matr_U[N].shape[0]
                     n_A += matr_U[N - m + 1].shape[1]
 
-                    temp = np.array([[0 for _ in range(n_A)] for _ in range(m_A)])
+                    temp = np.array([[0 for _ in range(n_A)] for _ in range(m_A)], dtype=float)
 
                     m_pos = 0
                     n_pos = 0
@@ -188,7 +188,7 @@ def calc_ramaswami_matrices(matr_S: np.ndarray, matr_tilde_S: np.ndarray, vect_b
             a = [[]]
 
             if j != 0:
-                a = np.array([[0 for _ in range(j + 1)]])
+                a = np.array([[0 for _ in range(j + 1)]], dtype=float)
 
                 for k in range(M - j - 1, M):
                     a[0][k - M + j + 1] = vect_beta[0][k]  # beta_g_1 ???
@@ -197,7 +197,7 @@ def calc_ramaswami_matrices(matr_S: np.ndarray, matr_tilde_S: np.ndarray, vect_b
 
             for m in range(1, N):
                 if j == 0:
-                    temp = np.array([[0 for _ in range(m + 2)] for _ in range(m + 1)])
+                    temp = np.array([[0 for _ in range(m + 2)] for _ in range(m + 1)], dtype=float)
 
                     for l in range(m + 1):
                         temp[l][l + 1] = vect_beta[0][M - 1]
@@ -213,7 +213,7 @@ def calc_ramaswami_matrices(matr_S: np.ndarray, matr_tilde_S: np.ndarray, vect_b
                         n_A += matr_P1[k].shape[1]
 
                     n_A += a[0].shape[0]
-                    temp = np.array([[0 for _ in range(n_A)] for _ in range(m_A)])
+                    temp = np.array([[0 for _ in range(n_A)] for _ in range(m_A)], dtype=float)
 
                     m_pos = 1
                     n_pos = 1

streams.py
@@ -38,8 +38,8 @@ class MAPStream:
         :param transition_matr1: np.array or list with transition matrix 1
         """
 
-        self.transition_matrices = [np.array(transition_matr0)]
-        self.transition_matrices.append(np.array(transition_matr1))
+        self.transition_matrices = [np.array(transition_matr0, dtype=float)]
+        self.transition_matrices.append(np.array(transition_matr1, dtype=float))
         self.transition_matrices_sum = self.transition_matrices[0] + self.transition_matrices[1]
         gamma = system_solve(self.transition_matrices_sum)
         self.avg_intensity = r_multiply_e(np.dot(gamma, self.transition_matrices[1]))[0]
@@ -92,8 +92,8 @@ class BMAPStream:
         """
 
         self.q = q
-        self.transition_matrices = [np.array(matrD_0)]
-        self.matrD = np.array(matrD)
+        self.transition_matrices = [np.array(matrD_0, dtype=float)]
+        self.matrD = np.array(matrD, dtype=float)
         for k in range(1, n + 1):
             self.transition_matrices.append(self.matrD * (q ** (k - 1)) * (1 - q) / (1 - q ** 3))
 
@@ -101,7 +101,7 @@ class BMAPStream:
         for matr in self.transition_matrices:
             matrD_1_ += matr
         theta = system_solve(matrD_1_)
-        matrdD_1_ = np.array(copy.deepcopy(self.transition_matrices[1]))
+        matrdD_1_ = np.array(copy.deepcopy(self.transition_matrices[1]), dtype=float)
         for i in range(2, n + 1):
             matrdD_1_ += self.transition_matrices[i] * i
         self.avg_intensity = r_multiply_e(np.dot(theta, matrdD_1_))[0]
@@ -122,12 +122,12 @@ class BMAPStream:
         Sets BMAP transition_matrices
         :param transition_matrices: iterable of np.arrays
         """
-        self.transition_matrices = np.array(transition_matrices)
+        self.transition_matrices = np.array(transition_matrices, dtype=float)
         matrD_1_ = np.zeros(self.transition_matrices[0].shape)
         for matr in self.transition_matrices:
             matrD_1_ += matr
         theta = system_solve(matrD_1_)
-        matrdD_1_ = np.array(copy.deepcopy(self.transition_matrices[1]))
+        matrdD_1_ = np.array(copy.deepcopy(self.transition_matrices[1]), dtype=float)
         for i in range(2, len(self.transition_matrices)):
             matrdD_1_ += self.transition_matrices[i] * i
         self.avg_intensity = r_multiply_e(np.dot(theta, matrdD_1_))[0]
@@ -197,9 +197,9 @@ class BMMAPStream:
 
         self.q = q
         self.transition_matrices = [[] for _ in range(t_num)]
-        self.matrD_0 = np.array(matrD_0)
+        self.matrD_0 = np.array(matrD_0, dtype=float)
         # matrD_t = [0.7 * np.array(matrD), 0.3 * np.array(matrD)]
-        matrD_t = [0.99999999999 * np.array(matrD), 0.00000000001 * np.array(matrD)]
+        matrD_t = [0.99999999999 * np.array(matrD, dtype=float), 0.00000000001 * np.array(matrD, dtype=float)]
         # matrD_t = [0.0000000000000001 * np.array(matrD), 0.9999999999999999 * np.array(matrD)]
         for t in range(t_num):
             if n == 3:
@@ -300,8 +300,8 @@ class PHStream:
         :param repres_matr: np.array or list with representation matrix
         """
 
-        self.repres_vect = np.array(repres_vect)
-        self.repres_matr = np.array(repres_matr)
+        self.repres_vect = np.array(repres_vect, dtype=float)
+        self.repres_matr = np.array(repres_matr, dtype=float)
         self.repres_matr_0 = -r_multiply_e(self.repres_matr)
         self.avg_intensity = -la.inv(r_multiply_e(np.dot(self.repres_vect,
                                                          la.inv(self.repres_matr))))[0, 0]

two_priorities_qs.py
@@ -29,7 +29,8 @@ class TwoPrioritiesQueueingSystem:
         self.matr_hat_Gamma = np.array(np.bmat([[np.zeros((1, self.timer_stream.repres_matr_0.shape[1])),
                                                  np.zeros((1, self.timer_stream.repres_matr.shape[1]))],
                                                 [self.timer_stream.repres_matr_0,
-                                                 self.timer_stream.repres_matr]]))
+                                                 self.timer_stream.repres_matr]]),
+                                       dtype=float)
         self.I_WM = np.eye(self.queries_stream.dim_ * self.serv_stream.dim)
         self.I_W = np.eye(self.queries_stream.dim_)
         self.I_M = np.eye(self.serv_stream.dim)
@@ -160,7 +161,7 @@ class TwoPrioritiesQueueingSystem:
 
         print("Q_00 calculated")
 
-        return np.array(matrQ_00)
+        return np.array(matrQ_00, dtype=float)
 
     def _calc_Q_0k(self):
         """
@@ -238,7 +239,8 @@ class TwoPrioritiesQueueingSystem:
             blocks1k.append(last_block1)
 
             temp_matr = np.array(np.bmat([blocks0k,
-                                          blocks1k]))
+                                          blocks1k]),
+                                 dtype=float)
 
             matrQ_0k.append(temp_matr)
 
@@ -337,7 +339,8 @@ class TwoPrioritiesQueueingSystem:
         print("Q_0N calculated")
 
         return np.array(np.bmat([blocks0k,
-                                 blocks1k]))
+                                 blocks1k]),
+                        dtype=float)
 
     def _calc_Q_10(self):
         """
@@ -363,7 +366,8 @@ class TwoPrioritiesQueueingSystem:
         print("Q_10 calculated")
 
         return np.array(np.bmat([[block00, block01],
-                                 [block10, block11]]))
+                                 [block10, block11]]),
+                        dtype=float)
 
     def _calc_Q_iiprev(self):
         """
@@ -771,12 +775,14 @@ class TwoPrioritiesQueueingSystem:
 
     def calc_system_empty_proba(self, stationary_probas):
         r_multiplier = np.array(np.bmat([[e_col(self.queries_stream.dim_)],
-                                         [np.zeros((self.queries_stream.dim_ * self.serv_stream.dim, 1))]]))
+                                         [np.zeros((self.queries_stream.dim_ * self.serv_stream.dim, 1))]]),
+                                dtype=float)
         return np.dot(stationary_probas[0], r_multiplier)[0][0]
 
     def calc_system_single_query_proba(self, stationary_probas):
         r_multiplier = np.array(np.bmat([[np.zeros((self.queries_stream.dim_, 1))],
-                                         [e_col(self.queries_stream.dim_ * self.serv_stream.dim)]]))
+                                         [e_col(self.queries_stream.dim_ * self.serv_stream.dim)]]),
+                                dtype=float)
         return np.dot(stationary_probas[0], r_multiplier)[0][0]
 
     def calc_buffer_i_queries_j_nonprior(self, stationary_probas, i, j):
@@ -789,7 +795,8 @@ class TwoPrioritiesQueueingSystem:
                                                self.timer_stream.dim - 1) for l in range(j + 1, i + 1)]))
         r_multiplier = np.array(np.bmat([[np.zeros((block0_size, 1))],
                                          [e_col(block1_size)],
-                                         [np.zeros((block2_size, 1))]]))
+                                         [np.zeros((block2_size, 1))]]),
+                                dtype=float)
         return np.dot(stationary_probas[i],
                       r_multiplier)[0][0]
 
@@ -808,7 +815,8 @@ class TwoPrioritiesQueueingSystem:
         p_loss = np.dot(stationary_probas[0],
                         np.array(np.bmat([[self.I_W],
                                           [kron(self.I_W,
-                                                e_col(self.serv_stream.dim))]])))
+                                                e_col(self.serv_stream.dim))]]),
+                                 dtype=float))
         r_sum = np.dot(stationary_probas[1],
                        kron(kron(self.I_W,
                                  e_col(self.serv_stream.dim)),
@@ -841,7 +849,8 @@ class TwoPrioritiesQueueingSystem:
         p_loss = np.dot(stationary_probas[0],
                         np.array(np.bmat([[np.zeros((self.queries_stream.dim_, self.serv_stream.dim))],
                                           [kron(e_col(self.queries_stream.dim_),
-                                                self.I_M)]])))  # checked
+                                                self.I_M)]]),
+                                 dtype=float))  # checked
         r_sum = np.dot(stationary_probas[1],
                        kron(kron(e_col(self.queries_stream.dim_),
                                  self.I_M),

utils.py
@@ -83,7 +83,7 @@ def system_solve(matr):
     :param matr: np.array
     :return: np.array with vect
     """
-    matr_a = np.array(matr)
+    matr_a = np.array(matr, dtype=float)
 
     for i in range(matr_a.shape[0]):
         matr_a[i][0] = 1
@@ -104,9 +104,9 @@ def r_multiply_e(matr):
     :return: any type with the result of multiplication
     """
     if len(matr.shape) > 1:
-        vect_e = np.array([[1.] for _ in range(matr.shape[1])])
+        vect_e = np.array([[1] for _ in range(matr.shape[1])], dtype=float)
     elif len(matr.shape) > 0:
-        vect_e = np.array([[1.] for _ in range(matr.shape[0])])
+        vect_e = np.array([[1] for _ in range(matr.shape[0])], dtype=float)
     else:
         vect_e = 1.
     result = np.dot(matr, vect_e)
@@ -120,7 +120,7 @@ def e_col(dim):
     :param dim: int dimension
     :return: np.array with unitary vector-column
     """
-    return np.array([[1.] for _ in range(dim)])
+    return np.array([[1] for _ in range(dim)], dtype=float)
 
 
 def matr_print(matr, file=sys.stdout):